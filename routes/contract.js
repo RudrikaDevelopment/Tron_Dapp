50c691185944fbfce3a95b4b7085024e04995eab
RudrikaFichadiya7 <rudrika.fichadiya@gmail.com>
2020-11-13 17:02:25 +0530

    Added getBalance and totalAupply APIs

--- routes/contract.js
+++ routes/contract.js
@@ -7,13 +7,13 @@ const HttpProvider = TronWeb.providers.HttpProvider;
 const fullNode = new HttpProvider("https://api.shasta.trongrid.io/");
 const solidityNode = new HttpProvider("https://api.shasta.trongrid.io/");
 const eventServer = new HttpProvider("https://api.shasta.trongrid.io/");
-const privateKey = "0aca16784a26552a317bdf9dd6dd566142634b4054ae78b65c29108565ab7d0e";
+const privateKey = "";
 const tronWeb = new TronWeb(fullNode,solidityNode,eventServer,privateKey);
 const trc20ContractAddress = "TRPCRk4o32zzSAiV8eFoaS9CwqshL6wiFR";//contract address
 
 
 async function triggerSmartContract(contractAddress, requestFor) {
-  //const trc20ContractAddress = "TRPCRk4o32zzSAiV8eFoaS9CwqshL6wiFR";//contract address
+  
   let contract = await tronWeb.contract().at(contractAddress);
   if(requestFor === "getTokenName"){
     try {
@@ -41,6 +41,28 @@ async function triggerSmartContract(contractAddress, requestFor) {
       console.error("trigger smart contract error:", e);
       return res.status(500).json({message: e.toString()});
     }
+  }
+  else if(requestFor === "getTokenTotalSupply"){
+    try {
+      let result = await contract.totalSupply().call();
+      let resultDecimal = BigInt(result._hex).toString();;
+      return resultDecimal;
+    }catch(e) {
+      console.error("trigger smart contract error:", e);
+      return res.status(500).json({message: e.toString()});
+    }
+  }
+}
+
+async function getBalance(contractAddress, address){
+  let contract = await tronWeb.contract().at(contractAddress);
+  try {
+    let result = await contract.balanceOf(address).call();
+    let balance = BigInt(result).toString();
+    return balance;
+  }catch(e) {
+    console.error("trigger smart contract error:", e);
+    return res.status(500).json({message: e.toString()});
   }
 }
 
@@ -136,6 +158,61 @@ router.get("/getTokenDecimals/:contractAddress",  (req, res, next) => {
       triggerSmartContract(contractData.contractAddress,req.url.split('/')[1])
       .then(data =>{
         return res.status(200).json({decimals: data});
+      })
+      .catch(err => {
+        return res.status(500).json({message: err.toString()});
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.toString()
+    });
+  });
+});
+
+/*  Method: To get token totalSupply of contract
+    Developed By: Rudrika Fichadiya
+    Date: 13/11/2020
+*/
+router.get("/getTokenTotalSupply/:contractAddress",  (req, res, next) => {
+
+  if(req.params.contractAddress === undefined || req.params.contractAddress == " "){
+    return res.status(200).json({message: "Please provide contract address"});
+  }
+  Contract.findOne({contractAddress: req.params.contractAddress})
+    .then(contractData => {
+      triggerSmartContract(contractData.contractAddress,req.url.split('/')[1])
+      .then(data =>{
+        return res.status(200).json({totalSupply: data});
+      })
+      .catch(err => {
+        return res.status(500).json({message: err.toString()});
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.toString()
+    });
+  });
+});
+
+/*  Method: To get balance of given address
+    Developed By: Rudrika Fichadiya
+    Date: 13/11/2020
+*/
+router.get("/getBalance/:contractAddress/:address",  (req, res, next) => {
+
+  if(req.params.contractAddress === undefined || req.params.contractAddress == " "){
+    return res.status(200).json({message: "Please provide contract address"});
+  }
+  else if(req.params.address === undefined || req.params.address == " "){
+    return res.status(200).json({message: "Please provide valid address"});
+  }
+  Contract.findOne({contractAddress: req.params.contractAddress})
+    .then(contractData => { 
+      getBalance(contractData.contractAddress,req.params.address)
+      .then(data => {
+        return res.status(200).json({address: req.params.address, balance: data});
       })
       .catch(err => {
         return res.status(500).json({message: err.toString()});
